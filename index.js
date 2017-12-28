c4c06f6a1d75ee0bcb93383b8e95a4121ded3229
Dumbbunny22 <34920883+Dumbbunny22@users.noreply.github.com>
2017-12-28 16:46:20 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+var prefix = '!'
+var CheckTimer;
+
+bot.on('message', (message) => {
+    if (message.content == prefix + 'check') {
+        message.channel.send('Timer Reset !');
+        clearInterval(CheckTimer);
+        CheckTimer = setInterval(CheckClock, 1800000);//1800000
+    } else if (message.content == prefix + 'stop') {
+        throw new Error('BOT STOPPED !');
+    } else if (message.content == prefix + 'start') {
+        CheckTimer = setInterval(CheckClock, 10000);
+    }
+
+});
+function CheckClock() {
+    bot.channels.get(bot.channels.find('name', 'wallcheck').id).send('@everyone CHECK WALLS CHECK WALLS CHECK WALLS');
+    bot.channels.get(bot.channels.find('name', 'wallcheck').id).send('@everyone CHECK WALLS CHECK WALLS CHECK WALLS');
+    bot.channels.get(bot.channels.find('name', 'wallcheck').id).send('@everyone CHECK WALLS CHECK WALLS CHECK WALLS');
+    bot.channels.get(bot.channels.find('name', 'wallcheck').id).send('@everyone CHECK WALLS CHECK WALLS CHECK WALLS');
+    bot.channels.get(bot.channels.find('name', 'wallcheck').id).send('@everyone CHECK WALLS CHECK WALLS CHECK WALLS');
+
+}
+
+bot.login('Mzk1NjY1MjYyNjQyMzMxNjQ4.DSYeDw.7nnMTuzkRAMGlEk-KUIzBbtalho');
